d58ca3523cd3da8b02a52d125f8d2a809d92b0d8
avdhoot111 <avdhootscience123@gmail.com>
2024-03-17 13:37:34 +0530

    Added Login and Logout APIs and Implemented Cookies

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const [auth, setAuth] = useState(false);
+    const [name, setName] = useState('');
+    axios.defaults.withCredentials = true;
+
+    useEffect(() => {
+        axios.get('http://localhost:7000')
+        .then((res) => {
+            console.log(res, 'response')
+            if(res.data.Status === 'Success') {
+                setAuth(true);
+                setName(res.data.name);
+            }else {
+                setAuth(false);
+            }
+        })
+        .catch((err) => console.log(err, 'error'));
+    })
+
+    const handleLogout = () => {
+        axios.get('http://localhost:7000/logout') 
+        .then((res) => {
+            window.location.reload();
+        })
+        .catch((err) => {
+            console.log('Error Logging out')
+        })
+    }
+
+    return (
+        <div className='container mt-4'>
+            {
+                auth ? 
+                <div>
+                    <h3>You are Authorised --- {name}</h3>
+                    <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+                </div> 
+                : 
+                <div>
+                    <h3>You are not Authenticated.</h3>
+                    <h3>Login Now</h3>
+                    <Link to="/login" className='btn btn-primary'>Login</Link>
+                </div>
+            }
+        </div>
+    )
+    }
 
 export default Home
--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const salt = 10;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use(cookieParser());
 
 const db = mysql.createConnection({
@@ -26,6 +30,25 @@ db.connect((err) => {
     }
     console.log('Connected to database');
 });
+
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    console.log(token, 'tokennnn')
+    if(!token) {
+        console.log('not token')
+        return res.status(500).json({ Error: 'You are not authenticated' });
+    }else {
+        console.log('token')
+        jwt.verify(token, "jwt-secrete-key", (err, decoded) => {
+            if(err) {
+                res.status(500).json({ Error: 'Token is not valid' });
+            }else {
+                req.name = decoded.name;
+                next();
+            }
+        })
+    }
+}
 
 app.post('/register', (req, res) => {
     const sql = "INSERT INTO login (name, email, password) VALUES ?";
@@ -44,6 +67,41 @@ app.post('/register', (req, res) => {
         });
     });
 });
+
+app.post('/login', (req, res) => {
+    const sql = 'SELECT * FROM login WHERE email = ?';
+    db.query(sql, [req.body.email], (err, data) => {
+        if(err) {
+            return res.status(500).json({ error: 'Error for Login' });
+        }
+        if(data.length > 0) {
+            console.log(req.body.password, data[0].password, 'input');
+            bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
+                console.log(response, err, 'response');
+                if(err) return res.json({Error: 'Hash Error'});
+                if(response) {
+                    const name = data[0].name;
+                    const token = jwt.sign({name}, 'jwt-secrete-key', {expiresIn: '1d'} ) //write secrete key in env file in production and real world practices!
+                    res.cookie('token', token)
+                    return res.json({Status: "Success"});
+                }else {
+                    return res.json({Error: "Password does not match"})
+                }
+            })
+        }else {
+            return res.json({Error: 'No email found'})
+        }
+    })
+})
+
+app.get('/', verifyUser, (req, res) => {
+    return res.json({Status: "Success", name: req.name})
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    return res.json({Status: "Success"})
+})
 
 app.listen(7000, () => {
     console.log('Running......!!');
